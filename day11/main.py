7ccca90604e2e75458390940f6865d6ecdaa3880
Ben Deane <ben@elbeno.com>
2025-12-11 20:56:26 -0700

    Day11 part 2

--- day11/main.py
+++ day11/main.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3
 
+from functools import cache
+
 
 def read_input(filename):
     graph = {}
@@ -15,26 +17,39 @@ def read_input(filename):
     return graph
 
 
-def star1(graph):
+graph_g = read_input("input")
+
+
+def star1():
     ways = 0
     q = ["you"]
     while len(q) != 0:
         head, *q = q
         if head == "out":
             ways += 1
-        elif head in graph:
-            q.extend(graph[head])
+        elif head in graph_g:
+            q.extend(graph_g[head])
     return ways
 
 
-def star2(graph):
-    return 0
+@cache
+def num_ways(start, end):
+    if start not in graph_g:
+        return 0
+    if end in graph_g[start]:
+        return 1
+    return sum(num_ways(n, end) for n in graph_g[start])
+
+
+def star2():
+    return num_ways("svr", "fft") * num_ways("fft", "dac") * num_ways(
+        "dac", "out"
+    ) + num_ways("svr", "dac") * num_ways("dac", "fft") * num_ways("fft", "out")
 
 
 def main():
-    graph = read_input("input")
-    print(f"star1: {star1(graph)}")
-    print(f"star2: {star2(graph)}")
+    print(f"star1: {star1()}")
+    print(f"star2: {star2()}")
 
 
 if __name__ == "__main__":
