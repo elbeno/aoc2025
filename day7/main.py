4e0db14b718b39bce107d272c01cd6e54de9e60b
Ben Deane <ben@elbeno.com>
2025-12-12 18:06:26 -0700

    Day7 star 2 Python

--- day7/main.py
+++ day7/main.py
@@ -41,10 +41,25 @@ def star1(grid):
     return splits
 
 
+def advance2(beams, splitters):
+    for i in range(len(beams)):
+        if splitters[i] == '^':
+            beams[i-1] += beams[i]
+            beams[i+1] += beams[i]
+            beams[i] = 0
+    return beams
+
+
+def star2(grid):
+    beams = [1 if c == '|' else 0 for c in grid[0]]
+    return sum(reduce(advance2, grid[1:], beams))
+
+
 def main():
     grid = read_input("input")
     grid[0] = grid[0].replace("S", "|")
     print(f"star1: {star1(grid)}")
+    print(f"star2: {star2(grid)}")
 
 
 if __name__ == "__main__":
